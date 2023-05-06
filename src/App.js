9c885f9ac8e84e5c077b66dd6736f9025cf5089d
SantoshChapagai <santoshchapagai100@gmail.com>
2023-05-06 09:33:09 +0300

    calling pokedex api and mapping to get name

--- src/App.js
+++ src/App.js
@@ -1,37 +1,16 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import Card from './Components/Card'
-import Main from './Components/Main';
-import './Components/Style.css'
+import Poke from './Components/Poke'
 import './App.css';
 
 function App() {
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  console.log(data);
 
-  useEffect(() => {
-    setIsLoading(true);
-    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then((response) => {
-      setData(response.data);
-      setIsLoading(false)
-    });
-
-  }, []);
-  if (isLoading) {
-    return <p>Loading...</p>
-  }
 
 
 
   return (
     <div className="App">
-      {data.results.map((item) => (< Card
-        name={item.name}
+      <Poke />
 
-      />))}
-      <Main />
     </div>
   );
 }
---
+++ src/Components/Poke.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Card from './Card'
+
+const Poke = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [searchInput, setSearchInput] = useState('');
+  console.log(data);
+
+  useEffect(() => {
+    setIsLoading(true);
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then((response) => {
+      const fetches = response.data.results.map((item) => {
+        return axios.get(item.url).then((response) => response.data)
+      });
+      Promise.all(fetches).then((response) => {
+        setData(response);
+        setIsLoading(false)
+      });
+
+    });
+
+  }, []); const searchInputHandler = (e) => {
+    setSearchInput(e.target.value)
+  }
+
+  const searchFilter = data.filter(item => {
+    return item.name.includes(searchInput)
+  })
+
+  if (isLoading) {
+    return <p>Loading...</p>
+  }
+  return (
+    <>
+      <input onChange={searchInputHandler} />
+      <div>
+        {searchFilter.map((item) => (<Card
+          key={item.name}
+          name={item.name}
+          image={item.sprites.other.dream_world.front_default}
+        />
+
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Poke;
